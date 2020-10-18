48165104557f737bcb29dd0a4bb9da6de55f2a19
unknown <omert1122@gmail.com>
2020-10-18 17:08:59 +0300

    first

---
+++ Cüzdanım App/Ekle.swift
@@ -0,0 +1,30 @@
+//
+//  Ekle.swift
+//  Cüzdanım App
+//
+//  Created by MacUser on 25.09.2018.
+//  Copyright © 2018 MacUser. All rights reserved.
+//
+
+import UIKit
+
+class Ekle: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
